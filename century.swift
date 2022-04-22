e94db69d9dd9f773cf08b0121d8d840a2d801f23
Jose Augusto <jose.malengo@hotmail.com>
2022-04-21 22:19:42 -0300

    Update century.swift
    create with a func

--- century.swift
+++ century.swift
@@ -1,16 +1,24 @@
 import Foundation
 
-let year = "100"
+
+func getCentury(_ year: String) -> String{
+
 let pos = year.count - 2
 
 if pos <= 0 {
-    print("Seculo 1")
+    return("Seculo 1")
     } else {
         let index = year.index(year.startIndex, offsetBy : pos)
         let num = Int (year[..<index])
              if year[index...] != "00" {
-                 print("Século \(num! + 1)" )
+                 return("Século \(num! + 1)" )
                  } else {
-                    print("Século \(num!) ")
+                    return("Século \(num!) ")
                     }
             }
+}
+print("Digite o Ano para Saber o século")
+let year  = readLine()
+if let text = year {
+print(getCentury(text))
+}
